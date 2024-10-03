69a3fd8869aec32e193510cf470c9c931f0eb9c2
Gabriel Fortes Ashikaga <gabriel.ashikaga@fatec.sp.gov.br>
2024-10-02 22:04:50 -0300

    :rocket: Fim da atividade

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Slider from "@react-native-community/slider";
+import { Picker } from "@react-native-picker/picker";
+import React, { Component } from "react";
+import {
+  Button,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: false,
+      sexo: 1,
+      sexos: [
+        {key: 1, sexo: "Masculino"},
+        {key: 2, sexo: "Feminino"},
+        {key: 3, sexo: "Não informar"},
+      ],
+      nome: "",
+      input: "",
+      valor: 0,
+    };
+    this.entrar = this.entrar.bind(this);
+
+  }
+
+  entrar() {
+    if (this.state.input === "") {
+      alert("Digite seu nome.");
+      return;
+    }
+    this.setState({
+      nome: "Conta Criada com sucesso! Bem vindo " + this.state.input +".",
+    });
+    alert(
+      "Conta Criada Com Sucesso!"
+    );
+  }
+
+  render() {
+
+    let sexoEscolhido = this.state.sexos.map((v) => {
+      return <Picker.Item key={v.key} label={v.sexo} value={v.key} />;
+    });
+    
+    return (
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Digite seu nome"
+          underlineColorAndroid="transparent"
+          style={styles.input}
+          onChangeText={(texto) => this.setState({ input: texto })}
+        />
+        <Text style={{ marginLeft: "auto", marginRight: "auto", fontSize: 20 }}>
+          Informe seu Sexo:
+        </Text>
+        <Picker
+          selectedValue={this.state.sexo}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ sexo: itemValue })
+          }
+        >
+          {sexoEscolhido}
+        </Picker>
+        <Text style={{ marginLeft: "auto", marginRight: "auto", fontSize: 20 }}>
+          Escolha seu Limite:
+        </Text>
+        <Slider
+          minimunValue={0}
+          maximumValue={100}
+          onValueChange={(valorSelecionado) =>
+            this.setState({ valor: valorSelecionado })
+          }
+          value={this.state.valor}
+          minimumTrackTintColor="#00FF00"
+          maximumTrackTintColor="#FF0000"
+        />
+        <Text style={{ textAlign: "center" }}>
+          {this.state.valor.toFixed(2)} R$
+        </Text>
+        <Switch
+          value={this.state.status}
+          onValueChange={(valorSwitch) =>
+            this.setState({ status: valorSwitch })
+          }
+          thumbColor="#FF0000"
+        />
+        <Text style={{ textAlign: "center", fontSize: 20 }}>
+          {this.state.status ? "Casado" : "Solteiro"}
+        </Text>
+        <Button title="Criar Conta" onPress={this.entrar}></Button>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "space-evenly"
+  },
+  input: {
+    height: 45,
+    borderWidth: 1,
+    borderColor: "#222",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 40,
+    fontSize: 20,
+    padding: 10,
+    width: "80%",
   },
 });
+
+export default App;
